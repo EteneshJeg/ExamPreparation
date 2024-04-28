48c59929338863dcab5763d1650e423bf893accf
Abuassefa7 <abuassefa19@gmail.com>
2024-04-28 12:41:15 -0500

    second

--- user/register.php
+++ user/register.php
@@ -1,31 +1,3 @@
-<!-- <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Register</title>
-</head>
-<body>
-    <h1>Register</h1>
-
-    <form action="/user/registrationHandler.php" method ="post">
-    <label for="fname">First Name:</label> <br>
-    <input type="text" name="name" > <br> <br>
-    <label for="lname">Last Name</label> <br>
-    <input type="text" name="lastname" > <br> <br>
-    <label for="email" >Email:</label> <br> 
-    <input type="email" name="email" > <br> <br>
-    <label for="password">Choose password:</label> <br> 
-    <input type="password" name="password"> <br> <br>
-    <input type="submit" value="Register">
-
-    </form>
-    
-</body>
-</html> -->
-
-
-
 
 <!DOCTYPE html>
 <html lang="en">
